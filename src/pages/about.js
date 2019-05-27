b9a98cadfe5cc29cd9e2a936f2125043813909c3
Ralph King Jr <ralph@ralphkingjr.com>
2019-05-27 16:44:40 -0400

    adding about blog and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me!</h1>
+      <h3>
+        I am currently a full-stack developer looking to improve my skillset. I
+        am freshly out of bootcamp and ready to grow my career
+      </h3>
+    </div>
+  )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello Ralph!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>
+        I am Ralph, a full-stack developer that wants to learn everything.
+      </h2>
+    </div>
+  )
+}
+
+export default IndexPage
